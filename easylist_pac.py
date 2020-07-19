a69ba544488cc82f802a1935625d5057eeb132b7
starraiderx <33199500+starraiderx@users.noreply.github.com>
2020-07-19 03:15:19 +0200

    Update easylist_pac.py

--- easylist_pac.py
+++ easylist_pac.py
@@ -119,12 +119,11 @@ class EasyListPAC:
         return self.args
 
     def easylists_download_latest(self):
-        iOSAdblockList = 'https://raw.githubusercontent.com/BlackJack8/iOSAdblockList/master/Hosts.txt'
         easylist_url = 'https://easylist.to/easylist/easylist.txt'
         easyprivacy_url = 'https://easylist.to/easylist/easyprivacy.txt'
         fanboy_annoyance_url = 'https://easylist.to/easylist/fanboy-annoyance.txt'
         fanboy_antifacebook = 'https://raw.githubusercontent.com/ryanbr/fanboy-adblock/master/fanboy-antifacebook.txt'
-        self.download_list = [iOSAdblockList, fanboy_antifacebook, fanboy_annoyance_url, easyprivacy_url, easylist_url] + self.extra_easylist_urls
+        self.download_list = [fanboy_antifacebook, fanboy_annoyance_url, easyprivacy_url, easylist_url] + self.extra_easylist_urls
         self.file_list = []
         for url in self.download_list:
             fname = os.path.basename(url)
